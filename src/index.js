b32520a34d6b6720aa48ad6bbd2e6b674bd44d6a
Arya-Pai <aryabhaskarpai@gmail.com>
2024-07-10 18:13:27 +0530

    Need to fix bootstrap bug not working ptoperly

--- src/index.js
+++ src/index.js
@@ -1,36 +1,41 @@
-const express=require('express');
-const app=express();
-const {Server}=require("socket.io");
-const http=require('http');
+const express = require('express');
+const app = express();
+const { Server } = require("socket.io");
+const http = require('http');
 const server = http.createServer(app);
-const io=new Server(server);
-const port=5000;
-const hbs=require('hbs');
+const io = new Server(server);
+const port = 5000;
+const hbs = require('hbs');
 const path = require('path');
+
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-app.set("view engine",'hbs');
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", 'hbs');
 app.set('views', path.join(__dirname, '../views'));
 app.use(express.static(path.join(__dirname, '../public')));
-app.use('/js', express.static(path.join(__dirname, './'))); // Serve script.js from src directory
+app.use('/js', express.static(path.join(__dirname, './')));
 
-app.get('/',(req,res)=>{
-    res.render('index');
-    
+app.get('/', (req, res) => {
+    res.render('index');  // Renders the index.hbs file
 });
-io.on('connection',(socket)=>{
-    console.log('user connected')
-    socket.on('send name',(username)=>{
-        io.emit('send name',(username));
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+
+    socket.on('send name', (username) => {
+        io.emit('send name', (username));
     });
-    
-    socket.on('send message',(chat)=>{
-        io.emit('send message',(chat));
+
+    socket.on('send message', (chat) => {
+        io.emit('send message', (chat));
     });
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 });
-server.listen(port,()=>{
+
+server.listen(port, () => {
     console.log(`Listening on port ${port}`);
-})
+});
